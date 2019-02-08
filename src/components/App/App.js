f1f143306c180ff21932b94f985485233b0a57a5
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2019-02-08 15:30:30 -0500

    Add dependancies

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { AppWrapper } from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <AppWrapper>
+        <h1>Hello!</h1>
+      </AppWrapper>
+    );
+  }
+}
+
+export default App;
